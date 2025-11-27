07a0643ec4262caba4599d9d60968995413598c1
tamtran28 <88757687+tamtran28@users.noreply.github.com>
2025-11-27 08:40:28 +0700

    Create seed_users.py

---
+++ db/seed_users.py
@@ -0,0 +1,11 @@
+from auth_db import init_db, create_user
+
+init_db()
+
+# Tạo 1 số user mẫu
+create_user("admin", "123456", "Quản trị hệ thống", "admin")
+create_user("kt_hn", "hn2025", "Kiểm toán Hà Nội", "audit")
+create_user("pos01", "pos2025", "User POS", "pos")
+create_user("the01", "the2025", "User Thẻ", "the")
+
+print("Đã tạo xong users.")
